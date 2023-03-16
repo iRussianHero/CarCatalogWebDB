1bd3db375e794f5e2ed3c3b5e437a07ff138a4b9
Roman Yakushev <boxboxbox@inbox.ru>
2023-03-16 21:00:58 +0300

    Добавьте файлы проекта.

---
+++ CarCatalogWebDB/Model/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CarCatalogWebDB.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<CarTable> CarTable { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            optionsBuilder.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
---
+++ CarCatalogWebDB/Model/CarTable.cs
@@ -0,0 +1,20 @@
+﻿namespace CarCatalogWebDB.Model
+{
+    public class CarTable
+    {
+        public string Id { get; set; }
+        public string CarName { get; set; }
+        public string BrandName { get; set; }
+        public int EngineVolume { get; set; }
+        public int ManufacturingYear { get; set; }
+
+        public CarTable()
+        {
+            Id = default;
+            CarName = "";
+            BrandName = "";
+            EngineVolume = 0;
+            ManufacturingYear = 1990;
+        }
+    }
+}
