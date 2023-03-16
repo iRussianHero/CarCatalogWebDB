980d75dc7f1ff5e3fd524aa501ea34a8ca9e6425
Roman Yakushev <boxboxbox@inbox.ru>
2023-03-16 21:25:28 +0300

    Add All dependisys

---
+++ CarCatalogWebDB/Migrations/20230316180623_init.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarCatalogWebDB.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CarTable",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    CarName = table.Column<string>(type: "text", nullable: false),
+                    BrandName = table.Column<string>(type: "text", nullable: false),
+                    EngineVolume = table.Column<int>(type: "integer", nullable: false),
+                    ManufacturingYear = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarTable", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CarTable");
+        }
+    }
+}
--- CarCatalogWebDB/Program.cs
+++ CarCatalogWebDB/Program.cs
@@ -1,6 +1,17 @@
+using CarCatalogWebDB.Model;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Welcome");
+
+app.MapGet("/all", async (ApplicationDbContext db) =>
+{
+    return await db.CarTable.ToListAsync();
+});
 
 app.Run();
