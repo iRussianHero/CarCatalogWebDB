923d27c4f5d1344034b70100b17689cf023809a7
Roman Yakushev <boxboxbox@inbox.ru>
2023-03-18 13:00:12 +0300

    Change DB to locale Docker

---
+++ CarCatalogWebDB/Migrations/20230318095630_NewPostgres.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarCatalogWebDB.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewPostgres : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
